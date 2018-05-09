f06fab9cddaa6ee7d64e45b900aab05fcb34bb05
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2018-05-09 14:14:37 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import counterReducer from './reducers/counterReducer';
 
 const store = createStore(counterReducer);
 
+<<<<<<< HEAD
 console.log(store.getState())
 store.dispatch({type: 'INCREMENT_COUNT'})
 store.dispatch({type: 'INCREMENT_COUNT'})
@@ -14,3 +15,15 @@ store.dispatch({type: 'INCREMENT_COUNT'})
 console.log(store.getState())
 store.dispatch({type: 'RESET_COUNT'})
 console.log(store.getState())
+=======
+console.log(store.getStore());
+store.dispatch({type: 'INCREMENT_COUNT'});
+store.dispatch({type: 'INCREMENT_COUNT'});
+console.log(store.getStore());
+store.dispatch({type: 'DECREMENT_COUNT'});
+console.log(store.getStore());
+store.dispatch({type: 'INCREMENT_COUNT'});
+store.dispatch({type: 'INCREMENT_COUNT'});
+store.dispatch({type: 'INCREMENT_COUNT'});
+console.log(store.getStore());
+>>>>>>> 229d6c55c72690e8df509fc5727fc918c58f9707
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,4 +1,5 @@
 const counterReducer = (state = 0, action) => {
+<<<<<<< HEAD
 	switch (action.type) {
 		case 'INCREMENT_COUNT':
 			return state + 1
@@ -9,6 +10,18 @@ const counterReducer = (state = 0, action) => {
 		default:
 			return state
 	}
+=======
+  switch (action.type) {
+    case 'INCREMENT_COUNT':
+      return state + 1;
+    case 'DECREMENT_COUNT':
+      return state - 1;
+    case 'RESET_COUNT':
+      return 0;
+    default:
+      return state;
+  }
+>>>>>>> 229d6c55c72690e8df509fc5727fc918c58f9707
 }
 
 export default counterReducer;
